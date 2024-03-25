c0eb80df9dea6f786df5f2a9070b12666e512e44
Vrudali Holey <73349942+Vrudali28@users.noreply.github.com>
2024-03-25 11:19:29 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-/*const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const dbConnection = require('./db')
-app.use(express.json())
-
-app.use('/api/bikes/' ,require('./routes/bikesRoute'))
-app.use('/api/users/' ,require('./routes/usersRoute'))
-app.use('/api/bookings/' ,require('./routes/bookingsRoute'))
-app.get('/',(req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Node js server started in port ${port}`))*/
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -30,5 +18,16 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Node.js server started on port ${port}`));
 
+/*const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const dbConnection = require('./db')
+app.use(express.json())
+
+app.use('/api/bikes/' ,require('./routes/bikesRoute'))
+app.use('/api/users/' ,require('./routes/usersRoute'))
+app.use('/api/bookings/' ,require('./routes/bookingsRoute'))
+app.get('/',(req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Node js server started in port ${port}`))*/
 
 
