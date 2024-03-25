be2dff7c18a23ef6fa0b8224f8451410a32ef7a9
Vrudali Holey <73349942+Vrudali28@users.noreply.github.com>
2024-03-25 11:12:18 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
@@ -8,6 +8,27 @@ app.use('/api/bikes/' ,require('./routes/bikesRoute'))
 app.use('/api/users/' ,require('./routes/usersRoute'))
 app.use('/api/bookings/' ,require('./routes/bookingsRoute'))
 app.get('/',(req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Node js server started in port ${port}`))
+app.listen(port, () => console.log(`Node js server started in port ${port}`))*/
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const dbConnection = require('./db');
+
+app.use(express.json());
+
+// Use CORS middleware to allow requests from all origins
+app.use(cors());
+
+// Routes
+app.use('/api/bikes/', require('./routes/bikesRoute'));
+app.use('/api/users/', require('./routes/usersRoute'));
+app.use('/api/bookings/', require('./routes/bookingsRoute'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Node.js server started on port ${port}`));
+
 
 
